8c4607c8cf3320bb570536b00b43d6f23c2062ad
kemselcuk <kemalselcuk07@gmail.com>
2023-03-03 21:53:35 +0300

    yeni odev yapiliyor

---
+++ src/BasamakSayiToplami.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class BasamakSayiToplami {
+    public static void main(String[] args) {
+        int n,b,basamak=0,sum=0,us=1,k;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Bir sayı girin: ");
+        n = sc.nextInt();
+        b = n;
+        while(b!=0){
+            b/=10;
+            basamak++;
+        }
+        for (int i = 1; i <= basamak; i++) {
+            for (int j = 1; j <=i  ; j++) {
+                us*=10;
+            }
+            if (us ==10) sum += n%(us);
+            else {
+                k = n%us;
+                sum+= k/(us/10);
+            }
+        }
+        System.out.print("Basamaklar toplamı= "+sum);
+    }
+}
